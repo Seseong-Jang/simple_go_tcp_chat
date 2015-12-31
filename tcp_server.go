20dbeafd2fa098fdfea0eb65c8e5ef6eb4af5b52
Seseong-Jang <genesis1343@nate.com>
2015-12-31 11:10:26 +0900

    add whisper and room

--- tcp_server.go
+++ tcp_server.go
@@ -11,8 +11,10 @@ import (
 )
 
 const (
-	LOGIN = "1"
-	CHAT  = "2"
+	LOGIN          = "1"
+	CHAT           = "2"
+	ROOM_MAX_USER  = 100
+	ROOM_MAX_COUNT = 50
 )
 
 type Client struct {
@@ -20,12 +22,22 @@ type Client struct {
 	read chan string
 	quit chan int
 	name string
+	room *Room
 }
 
-var clientlist *list.List
+type Room struct {
+	num        int
+	clientlist *list.List
+}
+
+var roomlist *list.List
 
 func main() {
-	clientlist = list.New()
+	roomlist = list.New()
+	for i := 0; i < ROOM_MAX_COUNT; i++ {
+		room := &Room{i + 1, list.New()}
+		roomlist.PushBack(*room)
+	}
 
 	ln, err := net.Listen("tcp", ":5000")
 	if err != nil {
@@ -55,8 +67,7 @@ func handleError(conn net.Conn, err error, errmsg string) {
 func handleConnection(conn net.Conn) {
 	read := make(chan string)
 	quit := make(chan int)
-
-	client := &Client{conn, read, quit, "unknown"}
+	client := &Client{conn, read, quit, "unknown", &Room{-1, list.New()}}
 
 	go handleClient(client)
 
@@ -67,8 +78,12 @@ func handleClient(client *Client) {
 	for {
 		select {
 		case msg := <-client.read:
-			if strings.HasPrefix(msg, "[B]") {
+			if strings.HasPrefix(msg, "[G]") {
 				sendToAllClients(client.name, msg)
+			} else if strings.HasPrefix(msg, "[R]") {
+				sendToRoomClients(client.room, client.name, msg)
+			} else if strings.HasPrefix(msg, "[W]") {
+				sendToClientToClient(client, msg)
 			} else {
 				sendToClient(client, client.name, msg)
 			}
@@ -99,13 +114,20 @@ func recvFromClient(client *Client) {
 	switch strmsgs[0] {
 	case LOGIN:
 		client.name = strings.TrimSpace(strmsgs[1])
+
+		room := allocateEmptyRoom()
+		if room.num < 1 {
+			handleError(client.conn, nil, "max user limit!")
+		}
+		client.room = room
+
 		if !client.dupUserCheck() {
 			handleError(client.conn, nil, "duplicate user!!"+client.name)
 			client.quit <- 0
 			return
 		}
-		fmt.Printf("\nhello = %s\n", client.name)
-		clientlist.PushBack(*client)
+		fmt.Printf("\nhello = %s, your room number is = %d\n", client.name, client.room.num)
+		room.clientlist.PushBack(*client)
 
 	case CHAT:
 		fmt.Printf("\nrecv message = %s\n", strmsgs[1])
@@ -126,29 +148,86 @@ func sendToClient(client *Client, sender string, msg string) {
 }
 
 func sendToAllClients(sender string, msg string) {
-	fmt.Printf("broad cast message = %s", msg)
-	for e := clientlist.Front(); e != nil; e = e.Next() {
-		c := e.Value.(Client)
-		sendToClient(&c, sender, msg)
+	fmt.Printf("global broad cast message = %s", msg)
+	for re := roomlist.Front(); re != nil; re = re.Next() {
+		r := re.Value.(Room)
+		for e := r.clientlist.Front(); e != nil; e = e.Next() {
+			c := e.Value.(Client)
+			sendToClient(&c, sender, msg)
+		}
 	}
 }
 
 func (client *Client) deleteFromList() {
-	for e := clientlist.Front(); e != nil; e = e.Next() {
-		c := e.Value.(Client)
-		if client.conn == c.conn {
-			clientlist.Remove(e)
+	for re := roomlist.Front(); re != nil; re = re.Next() {
+		r := re.Value.(Room)
+		for e := r.clientlist.Front(); e != nil; e = e.Next() {
+			c := e.Value.(Client)
+			if client.conn == c.conn {
+				r.clientlist.Remove(e)
+			}
 		}
 	}
 }
 
 func (client *Client) dupUserCheck() bool {
-	for e := clientlist.Front(); e != nil; e = e.Next() {
-		c := e.Value.(Client)
-		if strings.Compare(client.name, c.name) == 0 {
-			return false
+	for re := roomlist.Front(); re != nil; re = re.Next() {
+		r := re.Value.(Room)
+		for e := r.clientlist.Front(); e != nil; e = e.Next() {
+			c := e.Value.(Client)
+			if strings.Compare(client.name, c.name) == 0 {
+				return false
+			}
 		}
 	}
 
 	return true
+}
+
+func allocateEmptyRoom() *Room {
+	for e := roomlist.Front(); e != nil; e = e.Next() {
+		r := e.Value.(Room)
+
+		fmt.Printf("clientlist len = %d", r.clientlist.Len())
+		if r.clientlist.Len() < ROOM_MAX_USER {
+			return &r
+		}
+	}
+
+	// full room
+	return &Room{-1, list.New()}
+}
+
+func sendToRoomClients(room *Room, sender string, msg string) {
+	fmt.Printf("room broad cast message = %s", msg)
+	for e := room.clientlist.Front(); e != nil; e = e.Next() {
+		c := e.Value.(Client)
+		sendToClient(&c, sender, msg)
+	}
+}
+
+func findClientByName(name string) *Client {
+	for re := roomlist.Front(); re != nil; re = re.Next() {
+		r := re.Value.(Room)
+		for e := r.clientlist.Front(); e != nil; e = e.Next() {
+			c := e.Value.(Client)
+			if strings.Compare(c.name, name) == 0 {
+				return &c
+			}
+		}
+	}
+
+	return &Client{nil, nil, nil, "unknown", nil}
+}
+
+func sendToClientToClient(client *Client, msg string) {
+	strmsgs := strings.Split(msg, " ")
+
+	target := findClientByName(strmsgs[1])
+	if target.conn == nil {
+		fmt.Println("Can't find target User")
+		return
+	}
+
+	sendToClient(target, client.name, strmsgs[2])
 }
